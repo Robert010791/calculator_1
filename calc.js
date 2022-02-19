292fb8c07d0655b9ef29985292d508317e9e8d17
Robert Robbins <robertrobbins321@msn.om>
2022-02-19 16:44:31 -0700

    Square root implementation working

--- calc.js
+++ calc.js
@@ -13,6 +13,7 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	//sqRoot = Math.SQRT();
 
 
 	console.log({mathSymbol});
@@ -33,6 +34,10 @@ reader.question("What would you like to calculate?", function(input){
 
 	if(mathSymbol === "/"){
 		console.log(num1 / num2);
+	}
+
+	if(mathSymbol === "sqRoot"){
+		console.log(Math.sqrt(num1));
 	}
 
 	
