231b644050eb28d9bcdb5fca6410e6ad47d93a74
Robert Robbins <robertrobbins321@msn.om>
2022-02-19 15:40:22 -0700

    Addition implementaion working

--- calc.js
+++ calc.js
@@ -15,7 +15,21 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
+	console.log({mathSymbol});
+	console.log({num1});
+	console.log({num2});
+
+	if(mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+
+	
+
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
+
 });
+
+
